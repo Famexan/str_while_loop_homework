571dfb5a7ad47e161a33d93784dab8c8bfa152c6
fame <famephp@gmail.com>
2022-11-02 00:59:42 +0500

    01

--- While01.py
+++ While01.py
@@ -1,3 +1,13 @@
+ # i = len(s)
+    # count = 0
+    # while i > 0:
+    #     i -= 1
+    #     if s[i].isdigit():
+    #         count += 1
+
+    # if count == len(s):
+    #     s = int(s)
+    #     return s
 def main(s):
     """
     A variable of type str is given. Find how many numbers it contains and return.
@@ -6,16 +16,16 @@ def main(s):
     Returns:
         int: return answer
     """
-    i = len(s)
-    count = 0
-    while i > 0:
-        i -= 1
+
+    i = 0
+    count = ""
+
+    while i < len(s):
         if s[i].isdigit():
-            count += 1
-
-    if count == len(s):
-        s = int(s)
-        return s
-
+            count += s[i]
+        i += 1
+        
+    count = int(count)
+    return count
 
 
