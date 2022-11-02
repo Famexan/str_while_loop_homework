d42a773101f55e074e6a34b6fa1c433fe8aa5e46
fame <famephp@gmail.com>
2022-11-02 12:23:10 +0500

    05

--- While05.py
+++ While05.py
@@ -1,9 +1,20 @@
 def main(s):
     """
+    kichkina
     A variable of type str is given. Find how many lowercase letters there are and return.
     Args:
         s: str
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    count = ""
+
+    while i < len(s):
+        if s[i].islower():
+            count += s[i]
+        i += 1
+        
+    count = len(count)
+    
+    return count
